23f413f615409b38ab2e0242dd93701366ae7975
Ilya_Starushchanka <ilya10194@mail.ru>
2016-10-10 13:50:29 +0300

    producer

--- src/main/java/com/epam/bigdata/kafka/Producer.java
+++ src/main/java/com/epam/bigdata/kafka/Producer.java
@@ -31,9 +31,7 @@ public class Producer {
             paths.forEach(filePath -> {
                 if (Files.isRegularFile(filePath)) {
                     try(Stream<String> lines = Files.lines(filePath, Charset.forName("ISO-8859-1"))) {
-                        lines.forEach(line ->{
-                            producer.send(new ProducerRecord<>("logs-topic", line));
-                        });
+                        lines.forEach(line -> producer.send(new ProducerRecord<>("logs-topic", line)));
                     } catch (IOException ex) {
                         throw new RuntimeException(ex);
                     }
